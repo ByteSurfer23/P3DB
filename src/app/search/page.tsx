27463c55bcdd867a69ecb830d83f001403b88e75
Jabez Sam Sunder <jabezthedeveloper@gmail.com>
2025-09-04 15:23:28 +0530

    more small changes

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-export const dynamic = "force-no-store";
+export const dynamic = "force-dynamic";
 
 import React, { useEffect, useState } from "react";
 import { collection, getDocs, doc, getDoc, query, orderBy } from "firebase/firestore";
