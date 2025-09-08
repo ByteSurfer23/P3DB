90ae304c63fb7603e219c92d6b241033beb5630d
Jabez Sam Sunder <116026199+ByteSurfer23@users.noreply.github.com>
2025-09-08 17:01:08 +0530

    Update page.tsx

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -223,8 +223,8 @@ export default function SearchPage() {
               <SelectTrigger className="w-full"><SelectValue placeholder="Weight" /></SelectTrigger>
               <SelectContent className="z-50">
                 <SelectItem value="all">All</SelectItem>
-                <SelectItem value="with">With Weight</SelectItem>
-                <SelectItem value="without">Without Weight</SelectItem>
+                <SelectItem value="with">> 500 g/mol</SelectItem>
+                <SelectItem value="without">< 500 g/mol</SelectItem>
               </SelectContent>
             </Select>
           </div>
